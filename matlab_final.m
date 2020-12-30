9b513c61a4212a30160c08763ab3f510e22af8fe
Radwan <omarradwanx@gmail.com>
2020-12-30 11:05:28 +0200

    FM modulation

--- matlab_final.m
+++ matlab_final.m
@@ -1,51 +1,123 @@
 %-------------------------------------------------clear--------------------------------------------------------------------
-clc
-close all 
-clear all
+clc;
+close all;
+clear all;
 %-------------------------------------------------read file--------------------------------------------------------------------
 FILE = 'eric.wav';
-f_c = 100000;
-f_s_resampled = 5*f_c;
-[y_t, f_s]= audioread(FILE);
-%-------------------------------------------------message in frequency domain--------------------------------------------------------------------
-y_f = fftshift(fft(y_t));
-x_f = linspace(-f_s/2,f_s/2,length(y_f));
-%-------------------------------------------------plotting--------------------------------------------------------------------
-%plot_signal(x_f,real(y_f),'Message in frequency domain','Frequency','Value');
-%-------------------------------------------------filter--------------------------------------------------------------------
-filter = generate_filter(length(y_f),x_f,4000);
-y_f_filtered = filter.*y_f;
-%-------------------------------------------------plotting--------------------------------------------------------------------
-%plot_signal(x_f,real(filter),'Filter','Frequency','Value');
-%plot_signal(x_f,real(y_f_filtered),'Filtered message in frequency domain','Frequency', 'Value');
-%-------------------------------------------------back to time domain--------------------------------------------------------------------
-y_t_filtered= real(ifft(ifftshift(y_f_filtered)));
-%-------------------------------------------------sound--------------------------------------------------------------------
-%sound(y_t_filtered,f_s);
-%-------------------------------------------------plotting--------------------------------------------------------------------
-x_t = linspace(0,length(y_t_filtered)/f_s, length(y_t_filtered));
-%plot_signal(x_t, y_t_filtered, 'Filtered message in time domain','Time','Value'); 
-%-------------------------------------------------resmple--------------------------------------------------------------------
-y_t_resampled = resample(y_t_filtered,f_s_resampled,f_s);
-%-------------------------------------------------carrier signal--------------------------------------------------------------------
-t = linspace(0, (length(y_t_resampled)-1)/f_s_resampled, length(y_t_resampled)); %(x2-x1)/(n-1) = 1/5*fc
-carrier = cos(2*pi*f_c*t).';
-%-------------------------------------------------DSBSC modulation--------------------------------------------------------------------
-y_t_dsbsc = carrier.*y_t_resampled;
-y_f_dsbsc = fftshift(fft(y_t_dsbsc));
-x_t_dsbsc = t;
-x_f_dsbsc = linspace(-f_s_resampled/2,f_s_resampled/2,length(y_f_dsbsc));
-%-------------------------------------------------plotting--------------------------------------------------------------------
-%plot_signal(x_t_dsbsc,y_t_dsbsc,'DSBSC modulated signal in time domain', 'Time', 'Value');
-%plot_signal(x_f_dsbsc,real(y_f_dsbsc),'DSBSC modulated signal in frequency domain','Frequency','Value');
-%-------------------------------------------------DSBTC modulation--------------------------------------------------------------------
+fc = 100000;
+fs_res = 5*fc;
 modulation_index = 0.5;
-A = max(abs(y_t_resampled));
-y_t_dsbtc = A.*(1+modulation_index.*y_t_resampled).*carrier;
-y_f_dsbtc = fftshift(fft(y_t_dsbtc));
-x_t_dsbtc = t;
-x_f_dsbtc = linspace(-f_s_resampled/2,f_s_resampled/2,length(y_f_dsbtc));
+[yt, fs]= audioread(FILE);
+f_filter = 4000;
 %-------------------------------------------------plotting--------------------------------------------------------------------
-%plot_signal(x_t_dsbtc,y_t_dsbtc,'DSBTC modulated signal in time domain', 'Time', 'Value');
-%plot_signal(x_f_dsbtc,real(y_f_dsbtc),'DSBTC modulated signal in frequency domain','Frequency','Value');
-%-------------------------------------------------demodulation--------------------------------------------------------------------
+xt = linspace(0,length(yt)/fs,length(yt));
+%plot_signal(xt,yt,'Message in time domain','time','Value');
+%-------------------------------------------------message in frequency domain--------------------------------------------------------------------
+yf = fftshift(fft(yt));
+xf = linspace(-fs/2,fs/2,length(yf));
+%-------------------------------------------------plotting--------------------------------------------------------------------
+%plot_signal(xf,real(yf),'Message in frequency domain','Frequency','Value');
+%-------------------------------------------------filter--------------------------------------------------------------------
+filter = generate_filter(length(yf),xf,f_filter);
+yf_filtered = filter.*yf;
+%-------------------------------------------------plotting--------------------------------------------------------------------
+% plot_signal(xf,real(filter),'Filter','Frequency','Value');
+% plot_signal(xf,real(yf_filtered),'Filtered message in frequency domain','Frequency', 'Value');
+%-------------------------------------------------back to time domain--------------------------------------------------------------------
+yt_filtered= real(ifft(ifftshift(yf_filtered)));
+%-------------------------------------------------sound--------------------------------------------------------------------
+% sound(yt_filtered,fs);
+%-------------------------------------------------plotting--------------------------------------------------------------------
+xt = linspace(0,length(yt_filtered)/fs, length(yt_filtered));
+% plot_signal(xt, yt_filtered, 'Filtered message in time domain','Time','Value');
+%-------------------------------------------------resmple--------------------------------------------------------------------
+yt_resampled = resample(yt_filtered,fs_res,fs);
+%-------------------------------------------------carrier signal--------------------------------------------------------------------
+t = linspace(0,length(yt_resampled)/fs_res, length(yt_resampled)); %(x2-x1)/(n-1) = 1/5*fc, linspace(x1,x2,n)
+carrier = cos(2*pi*fc*t).';
+yt_carrier=carrier;
+yf_carrier=fftshift(fft(yt_carrier));
+xt_carrier=t;
+xf_carrier=linspace(-fc/2,fc/2,length(yt_carrier));
+%-------------------------------------------------plotting--------------------------------------------------------------------
+%plot_signal(xt_carrier, yt_carrier, 'carrier','','');
+%-------------------------------------------------DSBSC modulation--------------------------------------------------------------------
+yt_dsbsc = carrier.*yt_resampled;
+yf_dsbsc = fftshift(fft(yt_dsbsc));
+xt_dsbsc = t;
+xf_dsbsc = linspace(-fs_res/2,fs_res/2,length(yf_dsbsc));
+%sound(yt_dsbsc, fs);
+%-------------------------------------------------plotting--------------------------------------------------------------------
+%plot_signal(xt_dsbsc,yt_dsbsc,'DSBSC modulated signal in time domain', 'Time', 'Value');
+%plot_signal(xf_dsbsc,real(yf_dsbsc),'DSBSC modulated signal in frequency domain','Frequency','Value');
+%-------------------------------------------------DSBTC modulation--------------------------------------------------------------------
+A = max(abs(yt_resampled));
+message_normalized = yt_resampled./A;
+yt_dsbtc = A.*(1+modulation_index.*message_normalized).*carrier;
+yf_dsbtc = fftshift(fft(yt_dsbtc));
+xt_dsbtc = t;
+xf_dsbtc = linspace(-fs_res/2,fs_res/2,length(yf_dsbtc));
+%-------------------------------------------------plotting--------------------------------------------------------------------
+%plot_signal(xt_dsbtc,yt_dsbtc,'DSBTC modulated signal in time domain', 'Time', 'Value');
+
+%-------------------------------------------------DSBSC envelope detector demodulation--------------------------------------------------------------------
+yt_dsbsc_demod_ = abs(hilbert(yt_dsbsc));
+yt_dsbsc_demod_res_ = resample(yt_dsbsc_demod_ ,fs,fs_res);
+yf_dsbsc_demod_res_ = fftshift(fft(yt_dsbsc_dtc,real(yf_dsbtc),'DSBTC modulated signal in frequency domain','Frequency','Value');
+
+%-------------------------------------------------DSBSC coherent demodulation--------------------------------------------------------------------
+filter = generate_filter(length(yt_dsbsc),t,f_filter);
+yt_dsbsc_demod = (yt_dsbsc.*carrier).*filter;
+%plot_signal(t,yt_dsbsc_demod,'DSBSC demodulated signal in time domain', 'Time', 'Value');
+yt_dsbsc_demod_res = resample(yt_dsbsc_demod ,fs,fs_res);
+yf_dsbsc_demod_res = fftshift(fft(yt_dsbsc_demod_res));
+xf_dsbsc_demod_res = linspace(-fs/2,fs/2,length(yf_dsbsc_demod_res));
+%plot_signal(xf_dsbsc_demod_res, yf_dsbsc_demod_res, 'demodulated signal in frequency domain', 'Frequency', 'Value');
+%sound(yt_dsbsc_demod_res, fs);emod_res_));
+%sound(yt_dsbsc_demod_res_, fs);
+plot_signal(xf_dsbsc_demod_res, yf_dsbsc_demod_res_, 'DSBSC demodulated signal using envelope detector', 'Frequency', 'Value');
+%-------------------------------------------------DSBTC envelope detector demodulation--------------------------------------------------------------------
+yt_dsbtc_demod = abs(hilbert(yt_dsbtc));
+yt_dsbtc_demod_res = resample(yt_dsbtc_demod ,fs,fs_res);
+yf_dsbtc_demod_res = fftshift(fft(yt_dsbtc_demod_res));
+%sound(yt_dsbtc_demod_res, fs);
+%plot_signal(xf_dsbsc_demod_res, yf_dsbtc_demod_res, 'demodulated signal in frequency domain', 'Frequency', 'Value');
+%-------------------------------------------------modulation test-----------------------------------------------------------------
+[yt_modulated, t] = modulate(yt_resampled, fc, fs_res);
+yf_modulated = fftshift(fft(yt_modulated));
+f = linspace(-fc/2,fc/2,length(yf_modulated));
+%plot_signal(t,yt_modulated,'','','');
+%plot_signal(f,yf_modulated,'','','');
+
+yt_dsbtc = awgn(yt_dsbtc, 50);
+%-------------------------------------------------DSBTC envelope detector demodulation--------------------------------------------------------------------
+yt_dsbtc_demod = abs(hilbert(yt_dsbtc));
+yt_dsbtc_demod_res = resample(yt_dsbtc_demod ,fs,fs_res);
+yf_dsbtc_demod_res = fftshift(fft(yt_dsbtc_demod_res));
+sound(yt_dsbtc_demod_res, fs);
+%plot_signal(xf_dsbsc_demod_res, yf_dsbtc_demod_res, 'demodulated signal in frequency domain', 'Frequency', 'Value');
+%---------------------------------------------------------------------------------------------------------------
+receivedSignal = awgn(signal, 30);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- trials.m
+++ trials.m
@@ -1,45 +1,9 @@
 FILE = 'eric.wav';
-[y, Fs]= audioread(FILE);
-Y = fftshift(fft(y));
+x = linspace(0,2*pi,2*pi*1000);
+fc = 1;
+y = cos(2*pi*fc*x);
+yf = abs(fftshift(fft(y)));
+f = linspace(-fc/2,fc/2,2*pi*1000);
 
-%low pass filter
-N = length(y);
-numberOfOnes = floor(N * 8000 / Fs);
-numberOfZeros = floor((N - numberOfOnes) / 2);
-remainder = mod((N - numberOfOnes), 2);
-rect = ones(numberOfOnes, 1);
-filter = padarray(rect, numberOfZeros, 'pre');
-filter = padarray(filter, numberOfZeros + remainder, 'post');
-filteredSignalSpectrum = filter .* Y;
-filteredSignal = real(ifft(ifftshift(filteredSignalSpectrum)));
-sound(filteredSignal);
 
-message = filteredSignal;
-fc = 100000;
-carrierFs = 5 * fc;
-resampledMessage = resample(message, carrierFs, Fs);
-vectorLength = length(resampledMessage);
-time = linspace(0, vectorLength/carrierFs, vectorLength).';
-carrier = cos(2 * pi * fc * time);
-DSBSC = carrier .* resampledMessage;
-
-figure(1);
-plot(abs(fftshift(fft(DSBSC))));
-
-A = max(abs(resampledMessage));
-DSBTC = A.*(1+0.5.*resampledMessage).*carrier;
-figure(2);
-plot(abs(fftshift(fft(DSBTC))));
-
-%%demodulization ?
-reveivedSignal = DSBSC.*carrier; %same carrier assuming no error in phase or frequency (coherent)
-N = length(reveivedSignal);
-numberOfOnes = floor(N * 8000 / Fs);
-numberOfZeros = floor((N - numberOfOnes) / 2);
-remainder = mod((N - numberOfOnes), 2);
-rect = ones(numberOfOnes, 1);
-filter = padarray(rect, numberOfZeros, 'pre');
-filter = padarray(filter, numberOfZeros + remainder, 'post');
-filteredSignalSpectrum = filter .* reveivedSignal;
-filteredSignalDemod = real(ifft(ifftshift(filteredSignalSpectrum)));
-demodulatedSignal = resample(filteredSignalDemod, Fs, 5*fc);
+plot(f,yf);
